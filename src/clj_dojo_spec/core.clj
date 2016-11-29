06d80d383c4da16d614e9d910b5d55399fdae4bc
Christopher Evi-Parker <Chris.Parker@paymentexpress.com>
2016-11-29 20:43:03 +0000

    added slurping functionality

--- src/clj_dojo_spec/core.clj
+++ src/clj_dojo_spec/core.clj
@@ -1,6 +1,25 @@
 (ns clj-dojo-spec.core)
 
-(defn foo
+(defn trim
+  "trim string"
+  [string]
+  (clojure.string/trim string)
+  )
+
+(defn csv-parse [data]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split data #"\r\n")
+       )
+  )
+
+
+(defn read-presidents-to-map
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [filename]
+  (let [csv-data (slurp filename)]
+    (map #(map trim %)
+         (csv-parse csv-data)
+         )
+    )
+  )
+        
