2c00431bf794d6b247f84c4b659de781d148f27f
Craig Eales <craig.eales@gmail.com>
2016-11-29 19:43:06 +0000

    Initial empty lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-dojo-spec
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_dojo_spec/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-dojo-spec.core-test
+  (:require [clojure.test :refer :all]
+            [clj-dojo-spec.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
